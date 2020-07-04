7236fd9970b043b7f9f8ee1751e9e7da9c9d66af
0TheDragon0 <arianajavadi3@gmail.com>
2020-07-04 11:49:52 +0200

    Rewrote service from being Synchronous to being Asynchronous, Used observable in service and subscribed to it in component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -27,7 +27,12 @@ export class HeroesComponent implements OnInit {
     this.selectedHero = hero;
   }
 
+  //getHeroes(): void {
+  //this.heroes = this.heroService.getHeroes();
+  //}
+
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
 }
